7df310dcef1f5419ca064756f2f8ccbee2b069b1
hephzisoft <hephzisoft@gmail.com>
2022-08-29 09:36:48 +0100

    made additional changes and finished the about

--- base/templatetags/custom_tags.py
+++ base/templatetags/custom_tags.py
@@ -4,5 +4,11 @@ from django.urls import reverse
 register = template.Library()
 
 @register.simple_tag
-def anchor(home, about):
+def anchor(home, about,):
     return reverse(home) + '#' + about
+    
+
+
+@register.simple_tag
+def anchor(home, contact):
+    return reverse(home) + '#' + contact
